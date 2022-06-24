0aa0d9834e65190a0afe6e826aec8ec0c95530f1
andresguerrah <andres.guerrah@gmail.com>
2022-06-24 20:07:25 +0000

    tarea dom

--- src/app.js
+++ src/app.js
@@ -6,6 +6,10 @@ import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  //let pinta = ["♦", "♥", "♠", "♣"];
+  //let Numero = ["A", 2, 3, 4, 5, 6, 7, 8, 9, 10, "J", "Q", "K"];
+  let nram1 = document.querySelector("NUMEROCARTA"); //seleccionando contenido del h2
+  let N1 = document.createElement;
+
+  // hacer for .  le  t randomcard = "x"
 };
