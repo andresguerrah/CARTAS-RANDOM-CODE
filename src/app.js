f44148a4ab5ed953de401dc0a4cb1a023e9e4188
andresguerrah <andres.guerrah@gmail.com>
2022-06-24 20:41:26 +0000

    segundo avance tarea

--- src/app.js
+++ src/app.js
@@ -6,10 +6,26 @@ import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-  //let pinta = ["♦", "♥", "♠", "♣"];
-  //let Numero = ["A", 2, 3, 4, 5, 6, 7, 8, 9, 10, "J", "Q", "K"];
-  let nram1 = document.querySelector("NUMEROCARTA"); //seleccionando contenido del h2
-  let N1 = document.createElement;
+  let Pinta = ["♦", "♥", "♠", "♣"];
+  let Numero = [
+    "A",
+    "2",
+    "3",
+    "4",
+    "5",
+    "6",
+    "7",
+    "8",
+    "9",
+    "10",
+    "J",
+    "Q",
+    "K"
+  ];
+  const NUMERORAN = Numero[Math.floor(Math.random() * Numero.length)];
+  const PINTARAN = Pinta[Math.floor(Math.random() * Pinta.length)];
 
-  // hacer for .  le  t randomcard = "x"
+  var frase = (document.getElementById("NUMEROCARTA").innerHTML = NUMERORAN);
+  var frase = (document.getElementById("NUMEROCARTA2").innerHTML = NUMERORAN);
+  var frase2 = (document.getElementsByTagName("h3").innerHTML = PINTARAN);
 };
